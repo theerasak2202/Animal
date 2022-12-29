ec5ca5277ff57188e5c7e3d7e6331b96ad5d01ed
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-29 11:37:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ function App() {
   window.location="https://baibunviwe.com/2022/12/29/the-cairo-sl%cf%85m-appeared-a-mysterio%cf%85s-gia%d0%bft-stat%cf%85e-of-3000-years-old-26-feet-8-m-old-s%cf%85rprisi%d0%bfg-people/";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
