c1b5148d3c512edce47baade5a609789cb1677bf
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2023-01-01 16:13:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  window.location="https://baibunviwe.com/2022/12/29/the-cairo-sl%cf%85m-appeared-a-mysterio%cf%85s-gia%d0%bft-stat%cf%85e-of-3000-years-old-26-feet-8-m-old-s%cf%85rprisi%d0%bfg-people/";
+  window.location="https://baibunviwe.com/2023/01/01/test/";
   return (
     <div className="App">
       
